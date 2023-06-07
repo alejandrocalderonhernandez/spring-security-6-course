13ae552c00faa9c2f6894499d861c7b8700e1c82
alejandrocalderonhernandez <alejandro950825@gmail.com>
2023-06-07 13:45:26 -0600

    clean master

--- src/main/java/com/debuggeandoideas/springssecuritybasic/SpringSecurityApplication.java
+++ src/main/java/com/debuggeandoideas/springssecuritybasic/SpringSecurityApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringssecuritybasicApplication {
+public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringssecuritybasicApplication.class, args);
+		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
 
 }
